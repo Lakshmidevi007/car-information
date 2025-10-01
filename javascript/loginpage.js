1d4f69d465d18dad739395ea4b989250f6bc0677
Lakshmidevi007 <velmuruganseeman5@gmail.com>
2025-09-30 17:40:43 -0700

    first commit

--- javascript/loginpage.js
+++ javascript/loginpage.js
@@ -1,12 +1,12 @@
 function validateForm(){
    var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
- var confirmPassword=document.getElementById("confirmpassword").value;
+
 
  const items={}
  items.Name=username;
  items.Password=password;
- items.confirmpassword=confirmPassword;
+
  console.log(items)
  if(username===password){
    alert("login successfully")
--- javascript/upcomingcars.js
+++ javascript/upcomingcars.js
@@ -1,3 +1,6 @@
 function goToupcomingcars(){
     window.location.href="upcoming cars form.html";
 }
+function goToelectric(){
+    window.location.href="electric cars form.html";
+}
